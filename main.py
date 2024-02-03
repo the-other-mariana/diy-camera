d2409a0004f1875ceb18a6caf9a37be043f673a3
the-other-mariana <mariana.avalos.arce@gmail.com>
2024-02-03 13:19:54 -0600

    fix: use qt and take real pics
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-from picamera2 import Picamera2, Preview
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QApplication, QWidget
+from picamera2.previews.qt import QGlPicamera2
+from picamera2 import Picamera2
 from datetime import datetime
 from gpiozero import Button
-from tkinter import Button as TkButton, Tk, Label, PhotoImage
 import time
 
 class CameraSystem:
     def __init__(self):
         self.camera = None
+        self.camera_config = None
+        self.qpicamera2 = None
+        self.app = None
+        self.capture_button = None
+        self.window = None
+        self.layout_v = None
         self.set_up()
     def set_up(self):
         self.camera = Picamera2()
-        camera_config = self.camera.create_preview_configuration()
-        self.camera.configure(camera_config)
-        #self.camera.start_preview(Preview.QTGL)
-        #self.camera.start()
-    def show_menu(self):
-        print("pressed button")
+        self.camera_config = self.camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+        #self.camera_config = self.camera.create_preview_configuration()
+        self.camera.configure(self.camera_config)
+    def on_capture_button(self):
+        self.capture_button.setEnabled(False)
+        cfg = self.camera.create_still_configuration()
+        timestamp=datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.camera.capture_file(f"out/img_{timestamp}.jpg", signal_function=self.qpicamera2.signal_done)
+        #self.camera.switch_mode_and_capture_file(cfg, f"out/test_{timestamp}.jpg", signal_function=self.qpicamera2.signal_done)
+    def capture_done(self, job):
+        result = self.camera.wait(job)
+        self.capture_button.setEnabled(True)
+        
     def start(self):
-        self.camera.start_preview(Preview.QTGL)
-        self.camera.start()
-        root = Tk()
-        root.title("Digital Camera")
-        btn = TkButton(root, text="Button", command=self.show_menu())
-        btn.pack()
-        try: 
-            root.mainloop()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            self.camera.stop_preview()
-            self.camera.close()
+        self.app = QApplication([])
+        self.qpicamera2 =QGlPicamera2(self.camera, width=800, height=600, keep_ar=False)
+        self.capture_button = QPushButton("Capture")
+        self.window = QWidget()
+        self.qpicamera2.done_signal.connect(self.capture_done)
+        self.capture_button.clicked.connect(self.on_capture_button)
 
+        self.layout_v = QVBoxLayout()
+        self.layout_v.addWidget(self.qpicamera2)
+        self.layout_v.addWidget(self.capture_button)
+
+        self.window.setWindowTitle("Camera System")
+        self.window.resize(640, 480)
+        self.window.setLayout(self.layout_v)
+
+        self.camera.start()
+        self.window.show()
+        self.app.exec()
 def main():
     s = CameraSystem()
     s.start()
